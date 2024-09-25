455c65687efdf3a428fbefc2be205c587ab89ef9
Eduardo Andrade <edu3839@hotmail.com.noreply.github.com>
2024-09-25 10:24:58 -0300

    Alguns exercicios básicos que demonstram a linguagem c++

---
+++ exercicios-01/exercicio1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+  std::cout << "Vou " << ""
+  << "aprender " << ""
+   << "a programar " << ""
+     << "na linguagem " << ""
+      << "C++ " << "";
+  std::cin.get();
+  return 0;
+}
