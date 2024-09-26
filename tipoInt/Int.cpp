23d2385585691259b45ee1d616a1c16954e60964
Eduardo Andrade <edu3839@hotmail.com.noreply.github.com>
2024-09-26 16:47:46 -0300

    Amostra de exemplos e váriaveis do tipo int

---
+++ tipoInt/Int.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+  /*int Numero;
+  Numero = 32;
+  std::cout << "Valor: " << Numero << "\n" << "Endereço na memoria: " 
+  << &Numero << "\n" << "Tamanho da variável: " << sizeof(Numero) << "Bytes" << std::endl;
+  
+  std::cin.get();
+  return 0;*/
+  int NumComputadores, SerialComputadores;
+  NumComputadores = 100;
+  SerialComputadores = 11111;
+  std::cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << std::endl;
+  std::cout << "NumComputadores: " << NumComputadores << std::endl;
+  std::cout << "SerialComputadores: " << SerialComputadores << std::endl;  
+  std::cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << std::endl;
+  std::cin.get();
+  return 0;
+}
